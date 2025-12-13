10e6e906ae7678d487a180df91e08bfda6568de2
Ubuntu <ubuntu@sgx-vs01.informatik.uni-ulm.de>
2025-12-13 16:07:42 +0000

    update script

--- implementation/parameter_optimization/Splitting/pipeline.sh
+++ implementation/parameter_optimization/Splitting/pipeline.sh
@@ -10,20 +10,20 @@ pip install pandas scipy numpy scikit-learn tqdm matplotlib
 
 for zipfile in "$ZIP_DIR"/*.zip; do
     echo "--------------------------------------------"
-    echo "Bearbeite: $zipfile"
+    echo "Process: $zipfile"
     echo "--------------------------------------------"
 
-    # 1. Namen extrahieren
+    # 1. Name extraction
     NAME=$(basename "$zipfile" .zip)
     UNPACK_DIR="./temp_$NAME"
     mkdir -p "$UNPACK_DIR"
     
     if [[ ! "$NAME" =~ _[A-Za-z]+$ ]]; then
-        echo "Überspringe (kein Attack-Suffix): $NAME"
+        echo "Skipping due to none attack suffix: $NAME"
         continue
     fi
 
-    echo "Entpacke → $UNPACK_DIR"
+    echo "Unzip → $UNPACK_DIR"
     unzip -q "$zipfile" -d "$UNPACK_DIR"
     if [ -d "$UNPACK_DIR/$NAME" ]; then
       DATA_DIR="$UNPACK_DIR/$NAME"
@@ -31,7 +31,7 @@ for zipfile in "$ZIP_DIR"/*.zip; do
       DATA_DIR="$UNPACK_DIR"
     fi  
 
-    # 2. CSV Tabelle erzeugen
+    # 2. create CSV table
     echo "Running CSV Table Script"
     python3 csv_table.py -path "$DATA_DIR" -attack "$NAME"
 
@@ -40,11 +40,10 @@ for zipfile in "$ZIP_DIR"/*.zip; do
     echo "Running Train/Test Split"
     python3 train_test_dataset.py -file "$CSV_FILE" -attack "$NAME" -division "$TRAIN_RATIO"
 
-    # Erwartet: erzeugt z.B.
     TRAIN_CSV="${NAME}_train.csv"
     TEST_CSV="${NAME}_test.csv"
 
-    # 4. Dateien in korrektes Splitsystem kopieren
+    # 4. Save data to specified folders
     TRAIN_DST="$OUTPUT_ROOT/${NAME}/train/"
     TEST_DST="$OUTPUT_ROOT/${NAME}/test/"
     mkdir -p "$TRAIN_DST" "$TEST_DST"
@@ -59,19 +58,19 @@ for zipfile in "$ZIP_DIR"/*.zip; do
     rm ./*.csv
     echo "Results stored in: $OURPUT_ROOT"
 
-    # 5. Output ZIP erstellen
+    # 5. create output ZIP
     echo "ZIP Output"
     cd "$OUTPUT_ROOT"
     zip -rq "${NAME}_splitted.zip" "$NAME"
     cd -
 
-    # 6. Zwischendaten löschen
-    echo "Entferne entpackte Rohdaten & ungezippte Splits"
+    # 6. delete temporal data
+    echo "Delete temporal data"
     rm -rf "$UNPACK_DIR"
     rm -rf "$OUTPUT_ROOT/$NAME"
 
-    echo "Fertig: $NAME"
+    echo "Finished: $NAME"
     echo ""
 done
 
-echo "Fertig"
+echo "Processing done."
